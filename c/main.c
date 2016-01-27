a201c5b3fd614bea8cb5942af5af6714719a3a90
Thomas Willingham <thomas@tshows.us>
2016-01-27 04:16:47 +0300

    Added empty files for project

--- c/main.c
+++ c/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-int count;
-extern void write_extern();
+struct node {
+    int value;
+    struct node *left;
+    struct node *right;
+    struct node *parent;
+};
 
-main() {
-	count=5;
-	write_extern();
-}
+struct node *root = 0;
 
+
+
+/* program flow:
+ * 1) import data into tree
+ * 2) heapify tree
+ * 3) output integers
+ */
