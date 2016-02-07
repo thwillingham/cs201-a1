490cae78a5c1a06d86ff81700e9994dc4d6fc3c9
Thomas Willingham <thomas@tshows.us>
2016-02-07 05:55:56 +0300

    deleting unused files

--- treeNode.h
+++
@@ -1,9 +0,0 @@
-typedef struct tree_node_obj
-    {
-        int value;
-        struct node *parent;
-        struct node *left;
-        struct node *right;
-    } treeNode;
-
-extern treeNode *newTreeNode(int value, treeNode *parent, treeNode *left, treeNode *right);
